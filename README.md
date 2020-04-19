7c2be54ef136fc8b5301d4b66d234e0577dc2431
Fatimah <daviesfatimah97@gmail.com>
2020-04-18 22:15:53 -0700

    updated doc

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ Screenshot of successful database creation :
 Screenshot of successful interns collection creation :
 
 ![.exe files](https://raw.github.com/Fatimah019/mongodb-basics/master/images/internCollection.png)
+
+# crud-basics
+
+SCREENSHOT OF THE .EXE FILES (MONGO AND MONGOD) :
+
+![collection file](https://raw.github.com/Fatimah019/mongodb-basics/tree/crud-basics/images/movieCollection.png)
+
+SCREENSHOT OF THE .EXE FILES (MOVIE QUERIES) :
+
+![query file](https://raw.github.com/Fatimah019/mongodb-basics/trees/crud-basics/images/movieCollection.png)
--- updateInterns.js
+++ updateInterns.js
@@ -1,18 +1,21 @@
 const mongoClient=require('mongodb').MongoClient;
 const url="mongodb://localhost:27017/";
-const fs=require('fs');
-const internCollection=require('./interns.js');
 
 //connect to database
 mongoClient.connect(url, (err, db)=>{
     if (err) throw err;
-    var dbCol=db.db("Omowonuola");
-    //create collection
-
-        dbCol.collection('myMovies').updateOne({movie:"The Banker"}, 
-        {$set: {movie:"Miracle in cell n0.7", year:"2019", rating:9}}, function(err, result){
+    var dbCol=db.db("myMoviesCollection");
+    //update collection
+        dbCol.collection('myMovies').updateOne(
+            {movie:"The Banker"}, 
+            {
+                $set: {movie:"Miracle in cell n0.7", year:"2019", rating:9},
+                $currentDate:{lastModified:true}
+            }, function(err, res){
             if (err) throw err;
-            console.log(result);
+            console.log("updated collection, changed the bank document to miracles in cell no.7");
+            console.log(res);
             db.close();
         });
+
     });
